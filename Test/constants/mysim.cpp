db83abaa70d52105d66beeccd87161a11c8f2bb3
Konrad Winiarski <winek353@gmail.com>
2014-06-14 22:39:41 +0200

    constants test use new implementation

--- Test/constants/mysim.cpp
+++ Test/constants/mysim.cpp
@@ -1,12 +1,12 @@
 
 #include "mysim.hpp"
 
-KERNEL_IMP1( MySim, constants, IntArray, numbers)
+SIMSTEP_IMP1( MySim, constants, IntArray, numbers)
 {
   numbers[0] = IntegerConstant;
 }
 
-KERNEL_IMP1( MySim, float_constans, FloatArray, floats)
+SIMSTEP_IMP1( MySim, float_constans, FloatArray, floats)
 { 
   floats[0] = FloatConstant;
 }
--- Test/constants/mysim.hpp
+++ Test/constants/mysim.hpp
@@ -9,6 +9,6 @@ class MySim : public Simulation {
 public:
   MEMORY( IntArray, numbers, 5);
   MEMORY( FloatArray, floats, 3);
-  KERNEL_DEF1( constants, IntArray );
-  KERNEL_DEF1( float_constans, FloatArray );
+  SIMSTEP_DEF1( constants, IntArray );
+  SIMSTEP_DEF1( float_constans, FloatArray );
 };

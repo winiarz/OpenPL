273ec5092a3b7a5d717b635dfd1da1a91ff96e3b
Konrad Winiarski <winek353@gmail.com>
2014-06-15 12:17:30 +0200

    cleanup part 3

--- Makefiles/AutogenSource.mk
+++ Makefiles/AutogenSource.mk
@@ -1,9 +1,4 @@
 
-#Simulation Create Autogens
-SimCreateAutogenIncludes= ${shell $(SimCreate)/Script/scTypes.pl list_include $(SimCreate)}
-SimCreateAutogenSources=  ${shell $(SimCreate)/Script/scTypes.pl list_source  $(SimCreate)}
-SimCreateObjFromAutogen=  ${shell $(SimCreate)/Script/scTypes.pl list_obj     $(SimCreate)}
-
 #Simulation Execute Autogens
 SimExecuteAutogenIncludes= ${shell $(SimExecute)/Script/seTypes.pl list_include $(SimExecute)}
 SimExecuteAutogenSources=  ${shell $(SimExecute)/Script/seTypes.pl list_source  $(SimExecute)}
--- Simulation/SimulationCreate/SimCreate.mk
+++ Simulation/SimulationCreate/SimCreate.mk
@@ -15,46 +15,17 @@ SimCreateLibObj+=    $(SimCreateObj)/scSingleKernelSimStep.o
 .PRECIOUS: $(SimCreateLibObj) $(SimCreateAutogenIncludes) $(SimCreateAutogenSources) 
 
 filesToClean+= $(SimCreateObj)/*.o
-filesToClean+= $(SimCreateAutogenSources) $(SimCreateAutogenIncludes) $(SimCreateObjFromAutogen)
 
 $(libSimCreate): $(SimCreateLibObj) $(SimCreateObjFromAutogen)
 	@echo "\tLD\t"SimulationCreate.a
 	@ar rvs $@ $^ 2> /dev/null > /dev/null
 
-#autogen includes
-$(SimCreateInclude)/Types/scTypes.hpp: $(SimCreate)/Script/types.txt
-	@$(SimCreateTypeGen) gen_types $(SimCreate)
 
-$(SimCreateInclude)/Types/sc%Array.hpp: $(SimCreateTemplates)/scArray.hpp
-	@$(SimCreateTypeGen) gen_array_include $(SimCreate) $*
-
-$(SimCreateInclude)/Types/sc%.hpp: $(SimCreateTemplates)/scType.hpp
-	@$(SimCreateTypeGen) gen_type_include $(SimCreate) $*
-
-$(SimCreateInclude)/VecTypes/sc%Array.hpp: $(SimCreateTemplates)/scArray.hpp
-	@$(SimCreateTypeGen) gen_vector_array_include $(SimCreate) $*
-
-$(SimCreateInclude)/VecTypes/sc%.hpp: $(SimCreateTemplates)/scVector.hpp
-	@$(SimCreateTypeGen) gen_vector_include $(SimCreate) $*
-
-#autogen sources
-$(SimCreateSource)/Types/sc%Array.cpp: $(SimCreateTemplates)/scArray.cpp
-	@$(SimCreateTypeGen) gen_array_source $(SimCreate) $*
-
-$(SimCreateSource)/Types/sc%.cpp: $(SimCreateTemplates)/scType.cpp
-	@$(SimCreateTypeGen) gen_type_source  $(SimCreate) $*
-
-$(SimCreateSource)/VecTypes/sc%Array.cpp: $(SimCreateTemplates)/scVectorArray.cpp
-	@$(SimCreateTypeGen) gen_vector_array_source $(SimCreate) $*
-
-$(SimCreateSource)/VecTypes/sc%.cpp: $(SimCreateTemplates)/scVector.cpp
-	@$(SimCreateTypeGen) gen_vector_source $(SimCreate) $*
-
-$(SimCreateObj)/%.o: $(SimCreateSource)/%.cpp $(AllAutogenIncludes)
+$(SimCreateObj)/%.o: $(SimCreateSource)/%.cpp
 	@echo "\tCXX\t"$*.o
 	@g++ $< -o $@ $(AllInclude) $(cpp_flags)
 
-$(SimCreateObj)/%.o: $(SimCreateSource)/Structures/%.cpp $(AllAutogenIncludes)
+$(SimCreateObj)/%.o: $(SimCreateSource)/Structures/%.cpp
 	@echo "\tCXX\t"$*.o
 	@g++ $< -o $@ $(AllInclude) $(cpp_flags)
 

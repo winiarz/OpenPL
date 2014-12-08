2c5fcdfdb676bd581e49259022f7d6f1221aaea2
Konrad Winiarski <winek353@gmail.com>
2014-12-08 22:17:01 +0100

    add If_Command test to common makefile

---
+++ Test/if_command/If_Command.mk
@@ -0,0 +1,24 @@
+
+TestBinaries+=  Bin/Test/If_Command
+
+If_Command_Dir=Test/if_command
+If_Command_ObjDir=Obj/$(If_Command_Dir)
+
+If_Command_Obj+=$(If_Command_ObjDir)/mysim.o
+If_Command_Obj+=$(If_Command_ObjDir)/main.o
+
+If_Command_Includes+=$(If_Command_Dir)/mysim.hpp
+
+IfCommandTest: Bin/Test/If_Command
+	@echo "\tEXEC\tIf_CommandTest"
+	@Bin/Test/If_Command > /dev/null
+
+$(If_Command_ObjDir)/%.o: $(If_Command_Dir)/%.cpp $(allLibs)
+	@echo "\tCXX\t"$*.o
+	@mkdir -p $(dir $@) && $(cpp) $< -o $@ -I $(If_Command_Dir) $(AllInclude) $(cpp_flags)
+
+Bin/Test/If_Command: $(If_Command_Obj) $(If_Command_Includes) compile
+	@echo "\tLD\t"$@
+	@mkdir -p $(dir $@) && $(cpp) $(If_Command_Obj) $(allLibs) -o $@ $(OpenClLib) -lgtest -lgtest_main $(OpenGL) 
+
+filesToClean+=$(If_Command_Obj) Bin/Test/If_Command
--- makefile
+++ makefile
@@ -17,12 +17,13 @@ include Test/copy_integers/Copy_Integers.mk
 include Test/local_variables/Local_Variables.mk
 include Test/simple_aritmetics/SimpleAritmetics.mk
 include Test/constants/Constants.mk
+include Test/if_command/If_Command.mk
 
 compile: $(libClPlatform) $(libLogs) $(libSimCreate) $(libSimExecute) $(libInstructionProcessing)
 
 CompileTest: $(TestBinaries)
 
-test: ClPlatformTest InstructionProcessingTest CommonTest EmptySimulationTest DeclareMemoryTest CopyIntegersTest LocalVariablesTest SimpleAritmeticsTest ConstantsTest
+test: ClPlatformTest InstructionProcessingTest CommonTest EmptySimulationTest DeclareMemoryTest CopyIntegersTest LocalVariablesTest SimpleAritmeticsTest ConstantsTest IfCommandTest
 
 clean:
 	@rm -f lib/*.a $(filesToClean)

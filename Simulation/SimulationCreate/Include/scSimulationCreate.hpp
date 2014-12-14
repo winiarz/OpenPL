5129d88c902495148de8a3b6cb5f87021c9dfc35
Konrad Winiarski <winek353@gmail.com>
2014-12-14 21:27:48 +0100

    remove redundant methods

--- Simulation/SimulationCreate/Include/scSimulationCreate.hpp
+++ Simulation/SimulationCreate/Include/scSimulationCreate.hpp
@@ -11,14 +11,9 @@ namespace OPL
 {
   namespace SimCreate
   {
-    using namespace std;
-    using namespace OPL::SimCreate;
-    
     class Simulation {
     public:
       static InsPr::MatrixFloat3x3 invert(InsPr::MatrixFloat3x3);
-      static void print_opening_brace();
-      static void print_closing_brace();
     };
   }
 }
